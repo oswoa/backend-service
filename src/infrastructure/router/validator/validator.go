a1407e08762a52e8630255c98ec53f80b7aab467
osw <ows.oao@gmail.com>
2024-10-26 23:44:50 +0900

    レスポンスにもバリデーションを実施するよう修正

--- src/infrastructure/router/validator/validator.go
+++ src/infrastructure/router/validator/validator.go
@@ -1,23 +1,41 @@
 package validator
 
 import (
+	"fmt"
+
 	validator "github.com/go-playground/validator/v10"
+	"github.com/oswoa/backend-service/infrastructure/router/validator/model"
 	"github.com/oswoa/backend-service/internal_error/business_error"
 )
 
-// リクエストパラメータのバリデーションを実施する
-func RequestValidate(req interface{}) error {
+// パラメータのバリデーションを実施する
+func Validate(params interface{}) error {
+
 	validate := validator.New(validator.WithRequiredStructEnabled())
-	if validateErr := validate.Struct(req); validateErr != nil {
-		for _, err := range validateErr.(validator.ValidationErrors) {
-			return business_error.ValidateError{
-				FieldName: err.Field(),
-				ErrCode:   business_error.VALIDATE_ERROR_CODE,
-				ErrMsg:    business_error.VALIDATE_ERROR_MSG,
-				ErrTag:    err.Tag(),
+	if resultErr := validate.Struct(params); resultErr != nil {
+
+		for _, validateErr := range resultErr.(validator.ValidationErrors) {
+			switch params.(type) {
+			case model.ValidatorUserListRequest:
+				ret := business_error.RequestValidateError{
+					ErrCode:   business_error.ERR_CODE_VALIDATE_REQUEST,
+					ErrTag:    validateErr.Tag(),
+					FieldName: validateErr.Field(),
+				}
+				return ret
+
+			case model.ValidatorUserListResponse:
+				ret := business_error.ResponseValidateError{
+					ErrCode:   business_error.ERR_CODE_VALIDATE_RESPONSE,
+					ErrTag:    validateErr.Tag(),
+					FieldName: validateErr.Field(),
+				}
+				return ret
+
+			default:
+				return fmt.Errorf("不明なエラー")
 			}
 		}
 	}
-
 	return nil
 }
---
+++ src/internal_error/business_error/request_validate_error.go
@@ -0,0 +1,32 @@
+package business_error
+
+import (
+	"fmt"
+)
+
+// リクエストバリデーションエラー
+type RequestValidateError struct {
+	ErrCode   string
+	ErrTag    string
+	FieldName string
+}
+
+func (e RequestValidateError) Error() string {
+	return fmt.Sprintf(ERR_MSG_VALIDATE_REQUEST,
+		e.ErrCode,
+		e.FieldName,
+		e.GetErrDetail())
+}
+
+func (e RequestValidateError) GetErrDetail() string {
+	var errDetail string
+
+	switch e.ErrTag {
+	case "alphanum":
+		errDetail = "文字種が半角英数以外"
+	default:
+		panic(fmt.Sprintf("[%s]: リクエスト形式が不明な文字種です。", e.ErrTag))
+	}
+
+	return errDetail
+}
