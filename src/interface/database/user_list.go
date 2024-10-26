479fc40b013db48f595faf4e9e86f095ae05baa5
osw <ows.oao@gmail.com>
2024-10-26 18:41:00 +0900

    ユーザ一覧照会APIを修正

--- src/interface/database/user_list.go
+++ src/interface/database/user_list.go
@@ -1,11 +1,32 @@
 package database
 
 import (
+	"strings"
+
 	"github.com/oswoa/backend-service/interface/database/model"
 )
 
-func (d Database) UserList(whereQuery string, binds []interface{}, orderBy string) []model.Users {
+func (d Database) UserList(email string, isDeleted bool) []model.Users {
+
+	// queryの生成
+	whereQueries := make([]string, 0)
+	binds := make([]interface{}, 0)
+
+	if len(email) > 0 {
+		whereQueries = append(whereQueries, "email LIKE ?")
+		binds = append(binds, "%"+email+"%")
+	}
+
+	if !isDeleted {
+		whereQueries = append(whereQueries, "is_deleted = ?")
+		binds = append(binds, 0)
+	}
+	whereQuery := strings.Join(whereQueries, " AND ")
+
+	orderBy := "email"
+
 	var result []model.Users
 	d.conn.Query(&result, whereQuery, binds, orderBy)
+
 	return result
 }
