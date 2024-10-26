44da6482f77b83c5541e8fd44e88fba458d8d03e
osw <ows.oao@gmail.com>
2024-10-27 03:03:28 +0900

    TODOを追加

--- src/infrastructure/router/validator/model/get_user_authority.go
+++ src/infrastructure/router/validator/model/get_user_authority.go
@@ -6,10 +6,11 @@ type ValidatorGetUserAuthorityRequest struct {
 
 type ValidatorGetUserAuthorityResponse struct {
 	AuthorityName string `validate:"required"`
-	CanSearch     bool   `validate:"required,boolean"`
-	CanCreate     bool   `validate:"required,boolean"`
-	CanUpdate     bool   `validate:"required,boolean"`
-	CanDelete     bool   `validate:"required,boolean"`
-	CanApprove    bool   `validate:"required,boolean"`
-	CanPullBack   bool   `validate:"required,boolean"`
+	// TODO: gRPCの仕様でゼロ値の項目は返却されないため、ゼロ値に関わらず返却するようにする
+	CanSearch   bool `validate:"required,boolean"`
+	CanCreate   bool `validate:"required,boolean"`
+	CanUpdate   bool `validate:"required,boolean"`
+	CanDelete   bool `validate:"required,boolean"`
+	CanApprove  bool `validate:"required,boolean"`
+	CanPullBack bool `validate:"required,boolean"`
 }
